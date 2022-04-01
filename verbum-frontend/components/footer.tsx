7830d201905a92176784df73ca5611f8e6f06181
David Reis <david.alex.reis.97@gmail.com>
2022-04-02 00:03:26 +0100

    Tweak footer

--- verbum-frontend/components/footer.tsx
+++ verbum-frontend/components/footer.tsx
@@ -1,11 +1,11 @@
-import { Center, Text, Link } from "@chakra-ui/react";
+import { Center, Text, Link, Box } from "@chakra-ui/react";
 import Image from "next/image";
 
 const Footer = () => (
     <Center height="4em">
-        <Text>
-            Made in <Image title='Portugal' alt="Portugal" src="/portugal.svg" height={30 / 2} width={40 / 2} /> by&nbsp;
-        </Text>
+        <Text>Made in</Text>
+        <Box margin="0.25em 0.3em 0em 0.3em"><Image title='Portugal' alt="Portugal" src="/portugal.svg" height={30 / 2} width={40 / 2} /></Box>
+        <Text>by&nbsp;</Text>
         <Link isExternal textDecorationStyle="dashed" textDecorationThickness="1px" textDecorationLine="underline" href="http://www.davidreis.me">
             David Reis
         </Link>
