5580db95de16b43c82a2641664356928c61f24d0
David Reis <david.alex.reis.97@gmail.com>
2022-04-06 22:51:23 +0100

    Recycle rooms to make rooms more filled

--- verbum-gamehost/src/model/room_manager.go
+++ verbum-gamehost/src/model/room_manager.go
@@ -28,12 +28,22 @@ func (rm *RoomManager) StartRoomAndDispose(r *Room) {
 	//It should be up to the manager whether the room should be restarted or not. Would avoid having to pass the entire wordlist here.
 	rm.nextRoom.RunGame(rm.wordList)
 	rm.RemoveRoom(r.Id)
+	if r.Id == rm.nextRoom.Id {
+		rm.nextRoom = nil
+	}
 }
 
+// If there are no room, start a new room and set it as the next room.
+// If there are rooms and one of them has less than the max players, set it as the next room.
+// If there are rooms but all rooms are full, start a new room and set it as the next room.
 func (rm *RoomManager) GetNextRoom() *Room {
-	if rm.nextRoom == nil ||
-		len(rm.nextRoom.players) >= viper.GetInt("max_players") ||
-		(rm.nextRoom.state != Unstarted && rm.nextRoom.state != Starting) {
+	if rm.nextRoom == nil || len(rm.nextRoom.players) >= viper.GetInt("max_players") {
+		for room := range rm.rooms {
+			if len(rm.rooms[room].players) < viper.GetInt("max_players") {
+				rm.nextRoom = rm.rooms[room]
+				return rm.nextRoom
+			}
+		}
 
 		rm.nextRoom = NewRoom(rm.node, rm.wordList)
 		rm.rooms[rm.nextRoom.Id] = rm.nextRoom
